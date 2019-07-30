5d4b9f6910ca313cc9000658f1f273f22dce5f3f
Petrisor Neagu <petrisor@madball.ro>
2019-07-30 15:25:26 +0300

    submit method

--- js/core.js
+++ js/core.js
@@ -1,18 +1,46 @@
 function templateObject(){
     "use strict";
-    var formIdentity = '#formContact';
-    var formUrl = 'mod/submit.php';
+    var _formIdentity = '#formContact';
+    var _formUrl = 'mod/submit.php';
+
+    function _submitForm(){
+        "use strict";
+        $(document).on('submit', _formIdentity, function(e){
+            e.preventDefault();
+            e.stopPropagation();
+
+            var thisForm = $(this); // #formContact
+            var thisArray = thisForm.serializeArray(); // get all fields & values from the form
+
+            // send the form with ajax
+            $.post(_formUrl, thisArray, function(data){
+
+                if(data){
+                    if(!data.error){
+                        _clearFormValidation(thisForm);
+                        _displayMessage(thisForm, data.message);
+                        _resetForm(thisForm);
+
+                    }else if(data.validation){
+                        _clearFormValidation(thisForm);
+                        _displayMessage(thisForm, data.message);
+                        _validation(data.validation);
+
+                    }
+
+                }
+
+            },'json');
+
+        });
+    }
 
     this.init  = function() {
+
         "use strict";
 
-        function _submitForm(){
-            "use strict";
-        }
 
-
-        $(document).foundation(function(){
-
+        $(document).foundation(function() {
             _submitForm();
         });
 
@@ -25,4 +53,3 @@ $(function(){
     templateObj.init();
 
 });
-
