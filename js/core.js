23a5befe422310ee8caa4fbdd5c85c8791e3c054
Petrisor Neagu <petrisor@madball.ro>
2019-07-30 16:08:38 +0300

    <validation functions>

--- js/core.js
+++ js/core.js
@@ -3,6 +3,36 @@ function templateObject(){
     var _formIdentity = '#formContact';
     var _formUrl = 'mod/submit.php';
 
+
+    /**
+     * wrap whatever is coming from the ajax call into a markup of warning
+     * @param message
+     * @private
+     */
+    function _wrapValidation(message) {
+        "use strict";
+        return '<span class="warning">' + message + '</span>';
+    }
+
+
+    /**
+     * validates data from the form
+     * @param validation
+     * @private
+     */
+    function _validation(validation){
+        "use strict";
+
+        //iterate through all the inputs and display error if there is any
+        $.each(validation,function(k, v){
+           $('#' + k).before(_wrapValidation(v));
+        });
+
+
+
+    }
+    
+    
     function _submitForm(){
         "use strict";
         $(document).on('submit', _formIdentity, function(e){
